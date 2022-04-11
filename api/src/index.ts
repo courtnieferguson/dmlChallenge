5f510f1b701367db33ce6a96456c9711e4b01b69
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-04-11 12:33:16 -0500

    trying to add movies react app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,38 +1,38 @@
-import express from 'express'
-import cors from 'cors' 
+import express from "express";
+import cors from "cors";
 
-import { setupDb } from './sqlite'
+import { setupDb } from "./sqlite";
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 /**
  * Test ping, this is used by the UI to determine if they are connected properly.
  * Once you see the connected message on the UI, you can remove this if you want to.
  */
-app.get('/ping', (_, res) => {
-  res.send('pong')
-})
+app.get("/ping", (_, res) => {
+  res.send("pong");
+});
 
-app.get('/entity', (req, res) => {
+app.get("/entity", (req, res) => {
   // fetch many entities
-})
-app.post('/entity', (req, res) => {
+});
+app.post("/entity", (req, res) => {
   // create a single entity
-})
-app.delete('/entity/:id', (req, res) => {
+});
+app.delete("/entity/:id", (req, res) => {
   // delete a single entity
-})
-app.get('/entity/:id', (req, res) => {
+});
+app.get("/entity/:id", (req, res) => {
   // fetch a single entity
-})
+});
 
 app.listen(5010, async () => {
   try {
-    await setupDb()
-    console.log('🚀 Skynet is active')
+    await setupDb();
+    console.log("🚀 Skynet is active");
   } catch (e) {
-    console.error(e)
-    throw new Error('There was an issue setting up the DB')
+    console.error(e);
+    throw new Error("There was an issue setting up the DB");
   }
-})
+});
--- api/src/sqlite/seed.ts
+++ api/src/sqlite/seed.ts
@@ -1,18 +1,18 @@
-import { Entity } from '../interfaces'
+import { Entity } from "../interfaces";
 export const entities: Entity[] = [
   {
     id: 1,
-    name: 'Jim',
-    age: 34
+    name: "Jim",
+    age: 34,
   },
   {
     id: 2,
-    name: 'Pam',
-    age: 31
+    name: "Pam",
+    age: 31,
   },
   {
     id: 3,
-    name: 'Phyllis',
-    age: 41
-  }
-]
+    name: "Phyllis",
+    age: 41,
+  },
+];
